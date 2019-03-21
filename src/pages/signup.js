800c8a7a5c0f768a6793d443e4e57c8eb3c2d4ae
Adam Kiryk <akiryk@wayfair.com>
2019-03-21 16:51:58 -0400

    Add gitignore

---
+++ src/pages/signup.js
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from "react";
+
+class SignUp extends Component {
+  render() {
+    return (
+      <Fragment>
+        <h1>This is a signup page</h1>
+        <p>And here is a form...</p>
+      </Fragment>
+    );
+  }
+}
+
+export default SignUp;
