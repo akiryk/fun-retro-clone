bc47a86828f64a359b97f8e09f309d6d0dfcd8d0
Adam Kiryk <akiryk@wayfair.com>
2019-04-08 06:35:41 -0400

    Making a stories link

---
+++ src/pages/stories/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from '../../components/layout';
+
+const StoryPage = () => (
+  <Layout>
+    <h1>Stories</h1>
+    <p>Here there be list of stories</p>
+  </Layout>
+);
+
+export default StoryPage;
